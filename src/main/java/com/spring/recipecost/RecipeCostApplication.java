f1c8222f22348027f75900082e9e141f1d00ba91
oZanardo <joaovzanardo@gmail.com>
2026-05-02 19:18:58 -0300

    first commit

---
+++ src/main/java/com/spring/recipecost/RecipeCostApplication.java
@@ -0,0 +1,13 @@
+package com.spring.recipecost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeCostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeCostApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/recipecost/RecipeCostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.recipecost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeCostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
